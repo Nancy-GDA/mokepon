d917d4a41b45ffcda8abfeb417f9ca9b5ee14e86
Nancy-DeAnda <nancy.glez.deanda@gmail.com>
2022-07-25 15:27:27 -0500

    Eleccion aleatoria de mascotas

--- js/mokepon.js
+++ js/mokepon.js
@@ -3,12 +3,16 @@ function iniciarJuego (){
     btn_mascotas.addEventListener('click',seleccionarMascotas)
 }
 function seleccionarMascotas (){
+    selectionarMiMascota()
+    seleccionarMascotasEnemigo()
+}
+
+function selectionarMiMascota() {
     let item_charmelon = document.getElementById('charmeleon')
     let item_wartortle = document.getElementById('wartortle')
     let item_diglett = document.getElementById('diglett')
     let spanMascotaJugador = document.getElementById('mascota-jugador')
-    let spanMascotaEnemigo = document.getElementById('mascotas-enemigo')
-
+    
     if(item_charmelon.checked){
         spanMascotaJugador.innerHTML = 'Charmeleon'
     } else if (item_wartortle.checked){
@@ -18,8 +22,25 @@ function seleccionarMascotas (){
     } else{
         alert("Selecciona una mascota")
     }
-    
 }
+    
+function seleccionarMascotasEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascotas-enemigo')
+
+    if(ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Charmelon'
+    } else if (ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Wartortle'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Diglett'
+    }
+}
+
+function aleatorio(min,max){
+    return Math.floor(Math.random() * (max -min + 1) + min)
+}
+
 window.addEventListener('load',iniciarJuego)
 
 
